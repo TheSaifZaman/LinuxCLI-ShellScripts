9bf268484e6c926c52f5f43961690fda19c89da0
Md Saif Zaman <info.saifzaman@gmail.com>
2022-07-31 21:37:19 +0600

    Deleting and renameing old files script added

---
+++ ShellScripts/RealLifeShellScripts/rename_older_file.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: Md Saif Zaman
+# Date: July 31, 2022
+# Purpose: This script is to Rename older files
+# Modified: Null
+
+# Create a file with old time stamp to test: touch -d "Thu, 1 March 2018 12:30:00" a
+# ls -ltr
+find ./ -mtime +90 -exec ls -l {} \;
+find ./ -mtime +90 -exec mv {} {}.old \;
