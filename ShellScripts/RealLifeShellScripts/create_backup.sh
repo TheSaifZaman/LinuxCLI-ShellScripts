89a388601305f79fa238d8ae7e3c8847467b9320
Md Saif Zaman <info.saifzaman@gmail.com>
2024-01-27 12:34:37 +0600

    Backup Script Added

--- ShellScripts/RealLifeShellScripts/create_backup.sh
+++ ShellScripts/RealLifeShellScripts/create_backup.sh
@@ -9,14 +9,17 @@ tar cvf /tem/backup.tar /etc /var
 # Compress backup
 gzip backup.tar
 
+# Check that file exists.
 find backup.tar.gz -mtime -1 -type f -print &> /dev/null
 
+# If File exists do this.
 if [ $? -eq 0 ]
-then
+    then
     echo Backup was created
     echo
     echo Archiving backup
-
+    
+    # Send the file to right path
     scp /tmp/backup.tar.gz root@192.168.1.x:/path
 else
     echo Backup failed
